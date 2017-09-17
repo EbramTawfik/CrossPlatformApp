82cced92f942732c1e10f7226805989bc7f50c8f
Ebram Tawfik <engberosamy@yahoo.com>
2017-09-17 09:51:23 -0700

    WPF Project

---
+++ CrossPlatformApp/CrossPlatformApp.WPF/Views/MoviesView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using CrossPlatformApp.Infrastructure.ViewModels;
+using System.Windows.Controls;
+
+namespace CrossPlatformApp.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for MoviesView.xaml
+    /// </summary>
+    public partial class MoviesView : UserControl
+    {
+        MoviesViewModel Vm;
+        public MoviesView()
+        {
+            InitializeComponent();
+            Vm = new MoviesViewModel();
+            this.DataContext=Vm;
+            Vm.GetMoviesCommand.Execute(null);
+        }
+    }
+}
