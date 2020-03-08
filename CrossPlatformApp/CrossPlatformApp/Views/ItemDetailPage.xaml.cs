fc2459b3f5f97a95c997f176d901f6be706ebcb2
Ebram Tawfik <ebram.s.tawfik@gmail.com>
2020-03-07 16:47:02 -0800

    Updating Xamarin

---
+++ CrossPlatformApp/CrossPlatformApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using CrossPlatformApp.Infrastructure.ViewModels;
+
+namespace CrossPlatformApp.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class ItemDetailPage : ContentPage
+    {
+        MovieDetailsViewModel viewModel;
+        public ItemDetailPage(MovieDetailsViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+   
+    }
+}
---
+++ CrossPlatformApp/CrossPlatformApp/Views/ItemsPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using CrossPlatformApp.Infrastructure.ViewModels;
+using CrossPlatformApp.Infrastructure.Model;
+
+namespace CrossPlatformApp.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class ItemsPage : ContentPage
+    {
+        MoviesViewModel viewModel;
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new MoviesViewModel();
+        }
+
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            var item = args.SelectedItem as Movie;
+            if (item == null)
+                return;
+
+
+            await Navigation.PushAsync(new ItemDetailPage(new MovieDetailsViewModel(item)));
+            // Manually deselect item.
+            ItemsListView.SelectedItem = null;
+        }
+
+
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+
+            if (viewModel.MoviesList.Count == 0)
+                viewModel.GetMoviesCommand.Execute(null);
+        }
+    }
+}
